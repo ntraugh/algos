a68b4178a2773bae96da8ba9a1b7efcc48b97184
Nate Traugh <n.traugh@yahoo.com>
2022-09-05 15:53:47 -0400

    set up initial bubblesort algorithm practice

---
+++ bubbleSort.js
@@ -0,0 +1,15 @@
+const unsortedArray = []
+
+for (let i = 0; i < 10; i++) {
+    // random number between 0 & 1 times 2000, rounded up. 
+    // pushing that random number into unsortedArray to give us a bunch of random numbers that are unsorted
+    unsortedArray.push(Math.round(Math.random() * 1000))
+}
+
+const bubbleSort = (array) => {
+
+}
+
+
+
+console.log(unsortedArray)
