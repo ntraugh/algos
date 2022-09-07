f810d9454c3f482cec1359093126ca4cc09a3a87
Nate Traugh <n.traugh@yahoo.com>
2022-09-07 13:57:30 -0400

    created codeSignal file to work on problems, first one giving back 4 when I should get 3

---
+++ codeSignal.js
@@ -0,0 +1,14 @@
+//  Given two strings, find the number of common characters between them.
+function solution(s1, s2) {
+    let count = 0
+
+    for(let i = 0; i < s1.length; i++) {
+       if(s2.includes(s1[i])) {
+        s2.replace(s1[i], " ")
+        count++
+       }
+    }
+    return count
+}
+
+console.log(solution("aabcc", "adcaa"))
