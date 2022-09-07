0e2656dc22f657d0db54acf9211f7825803c9d29
Nate Traugh <n.traugh@yahoo.com>
2022-09-07 10:40:17 -0400

    create two empty arrays that we will store the values in, if it's less than pivot it goes into the left, greater than pivot goes to the right

---
+++ quickSort.js
@@ -0,0 +1,25 @@
+// Create our input data
+const unsortedInputArray = [];
+// seed data in unsortedInputArray
+for (let i = 0; i < 10; i++) {
+  unsortedInputArray.push(Math.round(Math.random() * 2000));
+}
+
+// TODO: create quick sort function
+const quickSort = (array) => {
+    // first need to check if the array is less than or equal to 1 in length, then it's already sorted
+    if(array.length <= 1) {
+        return array
+    }
+
+    // quick sort needs a pivot element to check the rest of the arrays elements against
+    const pivot = array.splice(Math.floor(Math.random() * array.length), 1)
+
+    // create two empty arrays that will store the values less than the pivot and greater than the pivot
+    const left = []
+    const right = []
+
+    
+};
+
+const sorted = quickSort(unsortedInputArray);
