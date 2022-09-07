90fd30a296e71912c825fd5db6921b49dc3d3416
Nate Traugh <n.traugh@yahoo.com>
2022-09-07 14:02:45 -0400

    forgot to reassign s2 inside the for loop, function now gives correct answers

--- codeSignal.js
+++ codeSignal.js
@@ -4,11 +4,11 @@ function solution(s1, s2) {
 
     for(let i = 0; i < s1.length; i++) {
        if(s2.includes(s1[i])) {
-        s2.replace(s1[i], " ")
+        s2 = s2.replace(s1[i], "")
         count++
        }
     }
     return count
 }
 
-console.log(solution("aabcc", "adcaa"))
+console.log(solution("abca", "xyzbac"))
