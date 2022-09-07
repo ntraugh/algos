7b4ea3e19f71b3d0b0ccc009da3d75720dee7345
Nate Traugh <n.traugh@yahoo.com>
2022-09-07 14:35:36 -0400

    finished second problem, passed all tests on codesignal but 'true, false' not printing in the console for some reason

--- codeSignal.js
+++ codeSignal.js
@@ -12,3 +12,26 @@ function solution(s1, s2) {
 }
 
 console.log(solution("abca", "xyzbac"))
+
+
+// Ticket numbers usually consist of an even number of digits. 
+// A ticket number is considered lucky if the sum of the first half of the digits is equal to the sum of the second half.
+// Given a ticket number n, determine if it's lucky or not.
+
+const isLucky = (n) => {
+    const myFunc = n => Number(n)
+    const numArray = Array.from(String(n), myFunc)
+    const middle = numArray.length / 2
+    const left = numArray.slice(0, middle)
+    const right = numArray.slice(middle)
+    const leftNumAdded = left.reduce((prev, curr) => prev + curr, 0)
+    const rightNumAdded = right.reduce((prev, curr) => prev + curr, 0)
+    if( leftNumAdded === rightNumAdded) {
+        return true
+    } else {
+        return false
+    }
+   
+}
+
+isLucky(100000)
