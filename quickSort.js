be5539a1750b0f04b6f6d3775756c13174d3a491
Nate Traugh <n.traugh@yahoo.com>
2022-09-07 13:28:55 -0400

    uncommented everythign else other than console.logs, renamed parameters to be more descriptive

--- quickSort.js
+++ quickSort.js
@@ -1,46 +1,46 @@
-// // Create our input data
-// const unsortedInputArray = [];
-// // seed data in unsortedInputArray
-// for (let i = 0; i < 10; i++) {
-//   unsortedInputArray.push(Math.round(Math.random() * 2000));
-// }
+// Create our input data
+const unsortedInputArray = [];
+// seed data in unsortedInputArray
+for (let i = 0; i < 10; i++) {
+  unsortedInputArray.push(Math.round(Math.random() * 2000));
+}
 
 
-// const quickSort = (array) => {
-//     // first need to check if the array is less than or equal to 1 in length, then it's already sorted
-//     if(array.length <= 1) {
-//         return array
-//     }
+const quickSort = (array) => {
+    // first need to check if the array is less than or equal to 1 in length, then it's already sorted
+    if(array.length <= 1) {
+        return array
+    }
 
-//     // quick sort needs a pivot element to check the rest of the arrays elements against
-//     const pivot = array.splice(Math.floor(Math.random() * array.length), 1)
+    // quick sort needs a pivot element to check the rest of the arrays elements against
+    const pivot = array.splice(Math.floor(Math.random() * array.length), 1)
 
-//     // create two empty arrays that will store the values less than the pivot and greater than the pivot
-//     const left = []
-//     const right = []
+    // create two empty arrays that will store the values less than the pivot and greater than the pivot
+    const left = []
+    const right = []
 
-//     array.forEach((el) => {
-//         if(pivot > el){
-//             left.push(el)
-//         } else {
-//             right.push(el)
-//         }
-//     });
+    array.forEach((el) => {
+        if(pivot > el){
+            left.push(el)
+        } else {
+            right.push(el)
+        }
+    });
 
-//     // now we need to call the quickSort function recursively since the array is probably not sorted on the first go round
-//     // we call quickSort on the left array, then concat the pivot and the right array
-//     // pivot is directly in the middle of array now because it's placed between the left and right arrays
-//     return quickSort(left).concat(pivot, quickSort(right));
+    // now we need to call the quickSort function recursively since the array is probably not sorted on the first go round
+    // we call quickSort on the left array, then concat the pivot and the right array
+    // pivot is directly in the middle of array now because it's placed between the left and right arrays
+    return quickSort(left).concat(pivot, quickSort(right));
 
-// };
+};
 
 // console.log(unsortedInputArray)
 // const sorted = quickSort(unsortedInputArray);
 // console.log(sorted)
 
-const findTheSpread = (ppg, ppg2, opposingPapg, opposingPapg2) => {
-    const firstTeamScore = (ppg + opposingPapg) / 2
-    const secondTeamScore = (ppg2 + opposingPapg2) / 2
+const findTheSpread = (team1ppg, team2ppg2, team2AllowedPapg, team1AllowedPapg) => {
+    const firstTeamScore = (team1ppg + team2AllowedPapg) / 2
+    const secondTeamScore = (team2ppg2 + team1AllowedPapg) / 2
 
     let final = firstTeamScore - secondTeamScore
     if(final < 0){
@@ -52,4 +52,4 @@ const findTheSpread = (ppg, ppg2, opposingPapg, opposingPapg2) => {
     
 }
 
-console.log(findTheSpread(28.4, 27.1, 21.9, 17))
+console.log(findTheSpread(22, 28.2, 27, 25.8))
