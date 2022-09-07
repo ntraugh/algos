3faeebb2e75b7bf0fc4886479dd63a6cd55b1755
Nate Traugh <n.traugh@yahoo.com>
2022-09-07 13:20:11 -0400

    created function to determine what a spread should be based on the points per game scored and the opponents points per game allowed

--- quickSort.js
+++ quickSort.js
@@ -1,37 +1,55 @@
-// Create our input data
-const unsortedInputArray = [];
-// seed data in unsortedInputArray
-for (let i = 0; i < 10; i++) {
-  unsortedInputArray.push(Math.round(Math.random() * 2000));
+// // Create our input data
+// const unsortedInputArray = [];
+// // seed data in unsortedInputArray
+// for (let i = 0; i < 10; i++) {
+//   unsortedInputArray.push(Math.round(Math.random() * 2000));
+// }
+
+
+// const quickSort = (array) => {
+//     // first need to check if the array is less than or equal to 1 in length, then it's already sorted
+//     if(array.length <= 1) {
+//         return array
+//     }
+
+//     // quick sort needs a pivot element to check the rest of the arrays elements against
+//     const pivot = array.splice(Math.floor(Math.random() * array.length), 1)
+
+//     // create two empty arrays that will store the values less than the pivot and greater than the pivot
+//     const left = []
+//     const right = []
+
+//     array.forEach((el) => {
+//         if(pivot > el){
+//             left.push(el)
+//         } else {
+//             right.push(el)
+//         }
+//     });
+
+//     // now we need to call the quickSort function recursively since the array is probably not sorted on the first go round
+//     // we call quickSort on the left array, then concat the pivot and the right array
+//     // pivot is directly in the middle of array now because it's placed between the left and right arrays
+//     return quickSort(left).concat(pivot, quickSort(right));
+
+// };
+
+// console.log(unsortedInputArray)
+// const sorted = quickSort(unsortedInputArray);
+// console.log(sorted)
+
+const findTheSpread = (ppg, ppg2, opposingPapg, opposingPapg2) => {
+    const firstTeamScore = (ppg + opposingPapg) / 2
+    const secondTeamScore = (ppg2 + opposingPapg2) / 2
+
+    let final = firstTeamScore - secondTeamScore
+    if(final < 0){
+        final = final * -1
+       return `+${final}` 
+    } else {
+        return `-${final}`
+    }
+    
 }
 
-// TODO: create quick sort function
-const quickSort = (array) => {
-    // first need to check if the array is less than or equal to 1 in length, then it's already sorted
-    if(array.length <= 1) {
-        return array
-    }
-
-    // quick sort needs a pivot element to check the rest of the arrays elements against
-    const pivot = array.splice(Math.floor(Math.random() * array.length), 1)
-
-    // create two empty arrays that will store the values less than the pivot and greater than the pivot
-    const left = []
-    const right = []
-
-    array.forEach((el) => {
-        if(pivot > el){
-            left.push(el)
-        } else {
-            right.push(el)
-        }
-    });
-
-    // now we need to call the quickSort function recursively since the array is probably not sorted on the first go round
-    // we call quickSort on the left array, then concat the pivot and the right array
-    // pivot is directly in the middle of array now because it's placed between the left and right arrays
-    return quickSort(left).concat(pivot, quickSort(right));
-
-};
-
-const sorted = quickSort(unsortedInputArray);
+console.log(findTheSpread(28.4, 27.1, 21.9, 17))
