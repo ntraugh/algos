c92c61db90607d793647db79e16239936c910cd5
Nate Traugh <n.traugh@yahoo.com>
2022-09-08 12:46:48 -0400

    codeWars problem for changing the letter of the alphabet to 13 letters from itself, rot13

--- codeSignal.js
+++ codeSignal.js
@@ -45,19 +45,40 @@ isLucky(100000)
 // be moved. Your task is to rearrange the people by their heights in a non-descending order
 // without moving the trees. People can be very tall!
 
-const solution2 = (a) => {
-    // loop through each index, if index is -1 skip it, sort the rest of the numbers
-    const sorted = [...a].sort((a, b) => a - b).filter((num) => num !== -1)
+const treeSort = (a) => {
+    // sorting the array and removing the -1 from the array for the time being
+    const sorted = [...a].sort().filter((num) => num !== -1)
+    console.log(sorted)
+
     let sortedIndex = 0
 
+    // standard for loop, looping through the original array "a"
     for(let i = 0; i < a.length; i++) {
+        // if that specific index is -1 we continue
         if(a[i] === -1) continue;
         else {
-            a[i] = sorted[sortedIndex++]
+            // if it's not -1 then we replace that index of array "a" with the sorted array at the first index "0" the first time
+            // then we increment sorted index to follow along with the loop
+            a[i] = sorted[sortedIndex]
+            sortedIndex++
         }
     }
     return a
 }
 
-console.log(solution2([-1, 150, 190, 170, -1, -1, 160, 180]))
+console.log(treeSort([-1, 150, 190, 170, -1, -1, 160, 180]))
 
+
+// ROT13 is a simple letter substitution cipher that replaces a letter with the letter 13 letters after it in the alphabet. 
+// ROT13 is an example of the Caesar cipher.
+
+const rot13 = (message) => {
+    const alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    
+    for(let i = 0; i < message.length; i++) {
+        message = message.replace(message[i], letter => alpha[alpha.indexOf(letter) + 13])      
+    }
+    return message
+}
+
+console.log(rot13("test"))
